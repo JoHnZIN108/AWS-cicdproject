342b3663d7628b4a9d557006fbe746a90c3e98f3
JoHnZIN108 <amahjohnson2@gmail.com>
2024-09-15 04:36:45 -0400

    .

--- cicdproject/cicdproject_stack.py
+++ cicdproject/cicdproject_stack.py
@@ -1,4 +1,5 @@
 from constructs import Construct
+from aws_cdk.pipelines import CodePipeline, CodePipelineSource, ShellStep
 from aws_cdk import (
     Duration,
     Stack,
@@ -6,6 +7,7 @@ from aws_cdk import (
     aws_sqs as sqs,
     aws_sns as sns,
     aws_sns_subscriptions as subs,
+    aws_codepipeline as codepipeline,
 )
 
 
@@ -13,3 +15,11 @@ class CicdprojectStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
+
+        # Creating a code pipeline
+        pipeline = CodePipeline(self, "MyPipeline",
+                        pipeline_name="MyPipeline",
+                        synth=ShellStep("SynthStep",
+                            input=CodePipelineSource.git_hub("joHnZIN108/cicdproject", "master"),
+                            commands=["npm install", "npm run build", "npx cdk synth"]
+                        ))
